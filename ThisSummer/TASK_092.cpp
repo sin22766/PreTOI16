87a130f6f8662cba4aa88a08d820b045d5ae2479
PCSHSCR <promchai2sin@gmail.com>
2020-03-31 13:31:35 +0700

    Remain 90 at BossRoom

--- ThisSummer/TASK_092.cpp
+++ ThisSummer/TASK_092.cpp
@@ -2,19 +2,44 @@
 
 using namespace std;
 
-int main() {
-    int n, m, temp, maxHP = 5000;
-    cin >> n >> m;
-    int **arr = new int *[n + 5];
-    for (int i = 1; i <= n; ++i) {
-        arr[i] = new int[n + 5];
+int dp[1010][1010], arr[1010][1010];
+
+int Solver(int n, int m) {
+    int minN = INT_MAX;
+    for (int i = n; i > 0; --i) {
         for (int j = 1; j <= m; ++j) {
-            cin >> temp;
-            arr[i][j] = temp * -1 + 500;
+            if (i == n) {
+                dp[i][j] = arr[i][j];
+            } else {
+                dp[i][j] = min(min(dp[i + 1][j], dp[i + 1][j - 1]), dp[i + 1][j + 1]) + arr[i][j];
+            }
         }
     }
-    for (int i = 1; i <= m; ++i) {
+    for (int k = 1; k <= m; ++k) {
+        if (dp[1][k] < minN) {
+            minN = dp[1][k];
+        }
+    }
+    return minN;
+}
 
+int main() {
+    int n, m, temp;
+    cin >> n >> m;
+    for (int i = 0; i <= n + 1; ++i) {
+        for (int j = 0; j <= m + 1; ++j) {
+            if (i > 0 && j > 0 && i <= n && j <= m) {
+                cin >> temp;
+                arr[i][j] = temp;
+            }
+            dp[i][j] = INT_MAX;
+        }
+    }
+    int result = Solver(n, m);
+    if (result < 5000 + n * 500) {
+        cout << result;
+    } else {
+        cout << "-1";
     }
     return 0;
 }
