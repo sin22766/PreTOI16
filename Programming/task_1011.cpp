372905e09d251583a0e93a79b14cd145a2368d00
PCSHSCR <promchai2sin@gmail.com>
2020-04-16 23:28:26 +0700

    Complete BlockGame

--- Programming/task_1011.cpp
+++ Programming/task_1011.cpp
@@ -2,15 +2,73 @@
 
 using namespace std;
 
-void gravity() {
+pair<int, int> mover[2] = {{1, 0},
+                           {0, 1}};//NEW
 
+int score = 0, row, col;
+char game[8][8];
+bool visited[8][8];
+
+void gravity(int &r, int &c, int &d) {
+    c += d;
+    while (game[r + 1][c] == '-') {
+        swap(game[r + 1][c], game[r][c]);
+        r++;
+    }
+}
+
+void gravityBelow(int r, int &c) {
+    while (r > 0 && game[r - 1][c] != '-' && game[r - 1][c] != '#') {
+        swap(game[r - 1][c], game[r][c]);
+        r--;
+    }
+}
+
+void bfs(int r, int c) {
+    vector<pair<int, int>> gravity_help;
+    char curr = game[r][c];
+    //Reset Visited
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < 5; ++j) {
+            visited[i][j] = false;
+        }
+    }
+
+    queue<pair<int, int>> q;
+
+    q.emplace(r, c);
+
+    while (!q.empty()) {
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+
+        if (visited[x][y]) {
+            continue;
+        }
+
+        visited[x][y] = true;
+        score += 5;
+        game[x][y] = '-';
+        gravity_help.emplace_back(x, y);
+
+        for (auto i:mover) {
+            if (x + i.first < row && y + i.second < col && game[x + i.first][y + i.second] == curr &&
+                !visited[x + i.first][y + i.second]) {
+                q.emplace(x + i.first, y + i.second);
+            }
+        }
+    }
+    for (auto i:gravity_help) {
+        gravityBelow(i.first, i.second);
+    }
 }
 
 int main() {
-    int row, col, com, score = 0, mover, r, c;
-    char dir;
+    int com, dir, r, c;
+    bool change;
+    char d;
     cin >> row >> col;
-    char game[row + 1][col + 1];
     for (int i = 0; i < row; ++i) {
         for (int j = 0; j < col; ++j) {
             cin >> game[i][j];
@@ -18,56 +76,26 @@ int main() {
     }
     cin >> com;
     for (int i = 0; i < com; ++i) {
-        cin >> r >> c >> dir;
-        mover = (dir == 'L') ? -1 : 1;
-        if (game[r][c] != '#' && game[r][c] != '-') {
-            if (game[r][c + mover] == '-') {
-                game[r][c + mover] = game[r][c];
-                game[r][c] = '-';
-                c += mover;
-                while (game[r + 1][c] == '-') {
-                    game[r + 1][c] = game[r][c];
-                    game[r][c] = '-';
-                    r++;
-                }
-                int x = 1, y = 1, temp;
-                bool horizontal = false;
-                while (game[r][c + x] == game[r][c] || game[r][c - y] == game[r][c]) {
-                    if (game[r][c + x] == game[r][c]) {
-                        game[r][c + x] = '-';
-                        temp = 0;
-                        while (r - temp > 0 && game[r - temp - 1][c + x] != '#' && game[r - temp - 1][c + x] != '-' &&
-                               game[r - temp][c + x] == '-') {
-                            swap(game[r - temp][c + x], game[r - temp - 1][c + x]);
-                            temp++;
+        cin >> r >> c >> d;
+        dir = (d == 'L') ? -1 : 1;
+        if (r >= 0 && c >= 0 && r < row && c < col && game[r][c] != '#' && game[r][c] != '-') {
+            if (game[r][c + dir] == '-') {
+                swap(game[r][c + dir], game[r][c]);
+                gravityBelow(r, c);
+                gravity(r, c, dir);
+                change = true;
+                while (change) {
+                    change = false;
+                    for (int j = 0; j < row; ++j) {
+                        for (int k = 0; k < col; ++k) {
+                            if (game[j][k] != '-' && game[j][k] != '#') {
+                                if (game[j][k] == game[j][k + 1] || game[j][k] == game[j + 1][k]) {
+                                    change = true;
+                                    bfs(j, k);
+                                }
+                            }
                         }
-                        x++;
-                        score += 5;
                     }
-                    if (game[r][c - y] == game[r][c]) {
-                        game[r][c - y] = '-';
-                        temp = 0;
-                        while (r - temp > 0 && game[r - temp - 1][c - y] != '#' && game[r - temp - 1][c - y] != '-' &&
-                               game[r - temp][c - y] == '-') {
-                            game[r - temp][c - y] = game[r - temp - 1][c - y];
-                            game[r - temp - 1][c - y] = '-';
-                            temp++;
-                        }
-                        y++;
-                        score += 5;
-                    }
-                    horizontal = true;
-                }
-                bool vertical = false;
-                while (game[r + 1][c] == game[r][c]) {
-                    game[r][c] = '-';
-                    r++;
-                    vertical = true;
-                    score += 5;
-                }
-                if (vertical || horizontal) {
-                    game[r][c] = '-';
-                    score += 5;
                 }
             } else {
                 score -= 5;
