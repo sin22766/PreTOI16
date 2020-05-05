0a6fe1c3a6785a3ad5296133fa2d3631b2916bd1
sin22766 <promchai2sin@gmail.com>
2020-05-05 13:42:08 +0700

    Complete De Fac

---
+++ ThisSummer/TASK_151.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n, result = 0;
+    scanf(" %d", &n);
+    int arr[n];
+    for (int i = 0; i < n; ++i) {
+        scanf(" %d", &arr[i]);
+    }
+    for (int i = n - 1; i >= 0; --i) {
+        if (result == 0) {
+            result = arr[i];
+        } else {
+            if (result % arr[i] != 0) {
+                result = ((result / arr[i]) + 1) * arr[i];
+            }
+        }
+    }
+    cout << result;
+    return 0;
+}
