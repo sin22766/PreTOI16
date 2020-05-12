073ce151b6828e71d6b54b033a082f5fa9123644
sin22766 <promchai2sin@gmail.com>
2020-05-13 00:28:09 +0700

    Woring on Dropping water

--- PreTOI16CodeCUbe/CUBE_223.cpp
+++ PreTOI16CodeCUbe/CUBE_223.cpp
@@ -3,10 +3,7 @@
 using namespace std;
 
 int main() {
-    pair<int, int> checker[3] = {{0,  -1},
-                                 {-1, -1},
-                                 {-1, 0}};
-    int n, m, sum = 0, minN;
+    int n, m, sum = 0;
     scanf(" %d %d", &n, &m);
     char temp[m + 10];
     int t = min(n, m);
@@ -17,22 +14,18 @@ int main() {
             scanf(" %s", &temp);
         }
         for (int j = 0; j <= m; ++j) {
+            if (!i || !j) {
+                pic[i][j] = 0;
+                continue;
+            }
             if (i && j && temp[j - 1] == '1') {
                 sum++;
-                minN = 1e9;
-                for (auto k:checker) {
-                    minN = min(minN, pic[i + k.first][j + k.second]);
-                }
-                pic[i][j] = minN + 1;
+                pic[i][j] = min(pic[i][j - 1], min(pic[i - 1][j], pic[i - 1][j - 1])) + 1;
                 ans[pic[i][j]]++;
             } else {
                 pic[i][j] = 0;
             }
-            if (j > 1) {
-                pic[i - 1][j - 2] = 0;
-            }
         }
-        pic[i - 1][n] = pic[i - 1][n - 1] = 0;
     }
     for (int T = 0; T < t; ++T) {
         sum -= ans[T];
