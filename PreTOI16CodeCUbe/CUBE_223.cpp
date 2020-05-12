3e307743669ad67920e5c9df7f274483288cebed
sin22766 <promchai2sin@gmail.com>
2020-05-13 00:32:41 +0700

    Woring on Dropping water

--- PreTOI16CodeCUbe/CUBE_223.cpp
+++ PreTOI16CodeCUbe/CUBE_223.cpp
@@ -2,22 +2,17 @@
 
 using namespace std;
 
+int pic[7005][7005];
+int ans[7005];
+
 int main() {
     int n, m, sum = 0;
     scanf(" %d %d", &n, &m);
     char temp[m + 10];
     int t = min(n, m);
-    int pic[n + 5][m + 5], ans[t + 5];
-    memset(ans, 0, t + 5);
-    for (int i = 0; i <= n; ++i) {
-        if (i) {
-            scanf(" %s", &temp);
-        }
-        for (int j = 0; j <= m; ++j) {
-            if (!i || !j) {
-                pic[i][j] = 0;
-                continue;
-            }
+    for (int i = 1; i <= n; ++i) {
+        scanf(" %s", &temp);
+        for (int j = 1; j <= m; ++j) {
             if (temp[j - 1] == '1') {
                 sum++;
                 pic[i][j] = min(pic[i][j - 1], min(pic[i - 1][j], pic[i - 1][j - 1])) + 1;
