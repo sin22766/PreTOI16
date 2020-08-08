c440504b6cd7541f7a082b03ef88892cc69cf41b
sin22766 <promchai2sin@gmail.com>
2020-08-08 22:27:02 +0700

    Dp Sliding windows

--- Programming/task_1157.cpp
+++ Programming/task_1157.cpp
@@ -3,29 +3,29 @@
 using namespace std;
 using pii = pair<int, int>;
 const int N = 500010;
-int A[N], dp[N];
+int A[N];
 
 int main() {
-    int n, k;
+    int n, k, dp;
     scanf("%d%d", &n, &k);
     for (int i = 1; i <= n; ++i)
         scanf("%d", &A[i]);
     deque<pii> Q;
-    dp[1] = A[1];
+    dp = A[1];
     for (int i = 2; i <= n; ++i) {
-        dp[i] = 1e9;
-        while (!Q.empty() && Q.back().first >= dp[i - 1]) {
+        while (!Q.empty() && Q.back().first >= dp) {
             Q.pop_back();
         }
 
-        Q.emplace_back(dp[i - 1], i - 1);
+        Q.emplace_back(dp, i - 1);
 
         while (!Q.empty() && Q.front().second < i - k) {
             Q.pop_front();
         }
-        dp[i] = Q.front().first + A[i];
+
+        dp = Q.front().first + A[i];
     }
-    printf("%d\n", dp[n]);
+    printf("%d\n", dp);
     return 0;
 }
 
